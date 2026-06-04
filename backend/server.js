3ec51f54732fd3eae8e41cf98126b9d5c0935f36
ghingsabucido-stack <ghingsabucido@gmail.com>
2026-06-04 16:40:12 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,42 @@
-console.log("NEW VERSION LOADED");
-
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+let products = [];
+
+console.log("NEW VERSION LOADED");
+
 app.get("/", (req, res) => {
   res.send("API WORKING");
+});
+
+// GET PRODUCTS
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+// ADD PRODUCT
+app.post("/api/products", (req, res) => {
+  const newProduct = {
+    id: Date.now(),
+    name: req.body.name,
+    stock: req.body.stock,
+    shop: req.body.shop
+  };
+
+  products.push(newProduct);
+  res.json(newProduct);
+});
+
+// DELETE PRODUCT
+app.delete("/api/products/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  products = products.filter(p => p.id !== id);
+  res.json({ message: "Deleted" });
 });
 
 const PORT = process.env.PORT || 8080;
